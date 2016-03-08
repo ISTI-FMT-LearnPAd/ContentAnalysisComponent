9bdda0e731b4f6d2f47c47e4677f3e8327684e2a
Giorgio <imatesiu@gmail.com>
2016-03-09 00:00:32 +0100

    persistence improved

--- src/main/java/eu/learnpad/ca/rest/impl/ColloborativeContentVerificationsImpl.java
+++ src/main/java/eu/learnpad/ca/rest/impl/ColloborativeContentVerificationsImpl.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -37,13 +36,9 @@ import eu.learnpad.ca.rest.ColloborativeContentVerifications;
 import eu.learnpad.ca.rest.data.collaborative.AnnotatedCollaborativeContentAnalyses;
 import eu.learnpad.ca.rest.data.collaborative.AnnotatedCollaborativeContentAnalysis;
 import eu.learnpad.ca.rest.data.collaborative.CollaborativeContentAnalysis;
-import eu.learnpad.ca.rest.data.stat.AnnotatedStaticContentAnalyses;
-import eu.learnpad.ca.rest.data.stat.AnnotatedStaticContentAnalysis;
-import eu.learnpad.ca.rest.data.stat.StaticContentAnalysis;
 import eu.learnpad.exception.LpRestException;
 
 @Path("/learnpad/ca/bridge")
-//@Consumes("text/plain")//{"text/plain", MediaType.APPLICATION_XML})
 @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 public class ColloborativeContentVerificationsImpl implements ColloborativeContentVerifications {
 
@@ -57,11 +52,12 @@ public class ColloborativeContentVerificationsImpl implements ColloborativeConte
 	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ColloborativeContentVerificationsImpl.class);
 
 
+	@SuppressWarnings("unchecked")
 	public ColloborativeContentVerificationsImpl(){
 		if(id<1){
 			try{
 				System.out.println("Entering ContactService.init()");
-				Query query = em.createNativeQuery("select ID FROM ANNOTATEDCOLLABORATIVECONTENTANALYSES order by ID");
+				Query query = em.createNativeQuery("select ID FROM ANNOTATEDCOLLABORATIVECONTENTANALYSES order by ID", Integer.class);
 				List<Integer> res = query.getResultList();
 				if(!res.isEmpty())
 					id =  res.get(res.size()-1);
@@ -295,144 +291,5 @@ public class ColloborativeContentVerificationsImpl implements ColloborativeConte
 
 		}
 	}
-
-	@Path("/validatestaticcontent")
-	@POST
-	public String putValidateStaticContent(StaticContentAnalysis contentFile)
-			throws LpRestException {
-		try{
-			String content = contentFile.getStaticContent().getContentplain();
-			if(content!=null && content.length()>0){
-				GateThread gateu = new GateThread(content,contentFile.getQualityCriteria());
-				gateu.start();
-				if(contentFile.getQualityCriteria().isCorrectness()){
-					id++;
-					Language lang = null;
-					if(contentFile.getLanguage()=="english"){
-						lang = new BritishEnglish();
-					}else{
-						if(contentFile.getLanguage().toLowerCase().equals("italian")){
-							lang = new Italian();
-						}else
-							if(contentFile.getLanguage().toLowerCase().equals("english uk")){
-								lang = new BritishEnglish();
-							}else
-								if(contentFile.getLanguage().toLowerCase().equals("english us")){
-									lang = new AmericanEnglish();
-								}else
-									lang = new BritishEnglish();
-					}
-					if(contentFile.getQualityCriteria().isCorrectness()){
-
-						CorrectnessAnalysis threadcorre = new CorrectnessAnalysis(lang, contentFile);
-						threadcorre.start();
-						putAndCreate(id, threadcorre);
-
-					}
-					if(contentFile.getQualityCriteria().isSimplicity()){
-
-						Simplicity threadEL = new Simplicity(contentFile, lang,gateu);
-						threadEL.start();
-						putAndCreate(id, threadEL);
-
-					}
-					if(contentFile.getQualityCriteria().isNonAmbiguity()){
-
-						NonAmbiguity threadNonAmbiguity = new NonAmbiguity (contentFile, lang, gateu);
-						threadNonAmbiguity.start();
-						putAndCreate(id, threadNonAmbiguity);
-
-					}
-					if(contentFile.getQualityCriteria().isContentClarity()){
-
-						ContentClarity threadContentClarity = new ContentClarity (contentFile, lang, gateu);
-						threadContentClarity.start();
-						putAndCreate(id, threadContentClarity);
-
-					}
-					if(contentFile.getQualityCriteria().isCompleteness()){
-
-						Completeness threadCompleteness = new Completeness (contentFile, lang);
-						threadCompleteness.start();
-						putAndCreate(id, threadCompleteness);
-
-					}
-					if(contentFile.getQualityCriteria().isPresentationClarity()){
-
-						PresentationClarity threadPresentation = new PresentationClarity (contentFile, lang);
-						threadPresentation.start();
-						putAndCreate(id, threadPresentation);
-
-					}
-					return id.toString();
-				}else{
-					log.error("No Content send: "+content);
-					return "No Content send";
-				}
-			}else{
-				log.error("Error "+"Null Element send");
-				return "Null Element send";
-			}
-
-		}catch(Exception e){
-			log.fatal("Fatal "+e.getMessage());
-			return "FATAL ERROR";
-		}
-	}
-
-
-	@Path("/validatestaticcontent/{idAnnotatedStaticContentAnalysis:\\d+}")
-	@GET
-	public AnnotatedStaticContentAnalyses getStaticContentVerifications(
-			@PathParam("idAnnotatedStaticContentAnalysis") String contentID) throws LpRestException {
-		try{
-			if(map.containsKey(Integer.valueOf(contentID))){
-				AnnotatedStaticContentAnalyses ar = new AnnotatedStaticContentAnalyses();
-				List<AbstractAnalysisClass> listanalysisInterface = map.get(Integer.valueOf(contentID));
-
-				for(AbstractAnalysisClass analysisInterface :listanalysisInterface){
-					AnnotatedStaticContentAnalysis annotatedStaticContent = analysisInterface.getAnnotatedStaticContentAnalysis();
-					if(annotatedStaticContent!=null){
-						annotatedStaticContent.setId(Integer.valueOf(contentID));
-						ar.setAnnotateStaticContentAnalysis(annotatedStaticContent);
-					}
-				}
-
-				return ar;
-			}else{
-				log.error("Element not found: "+contentID+" map:"+map.keySet().toString());
-				return null;
-			}
-		}catch(Exception e){
-			log.fatal("Fatal "+e.getMessage());
-			return null;
-		}
-	}
-
-	@Path("/validatestaticcontent/{idAnnotatedStaticContentAnalysis:\\d+}/status")
-	@GET
-	public String getStatusStaticContentVerifications(@PathParam("idAnnotatedStaticContentAnalysis") String contentID)
-			throws LpRestException {
-		try{
-			if(map.containsKey(Integer.valueOf(contentID))){
-				List<AbstractAnalysisClass> listanalysisInterface  = map.get(Integer.valueOf(contentID));
-				Integer progress = getProgress(listanalysisInterface);
-				if(progress>99)
-					return "OK";
-				else
-					return "InProgress_"+progress+"%";
-
-			}
-			log.error("Element not found: "+contentID+" map:"+map.keySet().toString());
-			return "ERROR";
-
-
-		}catch(Exception e){
-			log.fatal("Fatal "+e.getMessage());
-			return "FATAL ERROR";
-		}
-	}
-
-
 
 }
